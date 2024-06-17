db1fc9c65729a78984183fd56a5ae5e3386d7a95
Pavel Golovin <golovinp2014@mail.ru>
2024-06-17 20:49:31 +0300

    fix: added revalidatePath to getEmployees

--- src/app/(admin)/api/getEmployees/route.ts
+++ src/app/(admin)/api/getEmployees/route.ts
@@ -1,6 +1,13 @@
 import database from "@/database/database"
+import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+    const path = request.nextUrl.searchParams.get('path')
+ 
+    if (path) {
+      revalidatePath(path)
+    }
     const users = await database.models.Employee.findAll();
 
 
